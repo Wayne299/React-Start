3e51c1cb14fba38669fbe6526284b3331dbd9d2e
Wayne299 <673156454@qq.com>
2019-09-22 10:46:22 +0800

    add App.js

---
+++ pics/src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+	onSearchSubmit(term) {
+		axios.get("http://api.unsplash.com/search/photos", {
+			params: { query: term },
+			headers: {
+				Authorization:
+					"Client-ID 34e39e5c2f447ce52009a515846ca2b6ccc35552bb63de59cf4a6d06728f3f7e"
+			}
+		});
+	}
+
+	render() {
+		return (
+			<div className="ui container" style={{ marginTop: "10px" }}>
+				<SearchBar onSubmit={this.onSearchSubmit} />
+			</div>
+		);
+	}
+}
+
+export default App;
