b4af870595e75e5ea96bed81ccabcbc79cb41838
Wayne299 <673156454@qq.com>
2019-10-05 22:29:04 +0800

    add api

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const KEY = ""; // console.developers.google.com
+
+export default axios.create({
+	baseURL: "https://www.googleapis.com/youtube/v3",
+	params: {
+		part: "snippet",
+		maxResults: 5,
+		key: KEY
+	}
+});
