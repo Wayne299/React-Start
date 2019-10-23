1d7b8ccc7fc53e57f1b2b623cd2fe225d7828d25
Wayne299 <673156454@qq.com>
2019-10-23 23:56:44 +0800

    add authentic id

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: "http://api.unsplash.com/search/photos",
+	baseURL: "http://api.unsplash.com",
 	headers: {
 		Authorization:
 			"Client-ID 50d197b473f6ba8738f27373cc6c3830d56921aeecbb36766b160b0d52db21e8"
