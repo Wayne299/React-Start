5b7fe63777a679278f8a5a1cdc601cac543b6275
Wayne299 <673156454@qq.com>
2019-10-24 17:10:56 +0800

    delete authorization id

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
 	baseURL: "https://api.unsplash.com",
 	headers: {
-		Authorization:
-			"Client-ID 50d197b473f6ba8738f27373cc6c3830d56921aeecbb36766b160b0d52db21e8"
+		Authorization: "Client-ID xxxxx"
 	}
 });
