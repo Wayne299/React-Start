19d82bfbbb7db6112385d70267978fa6ec51578e
Wayne299 <673156454@qq.com>
2019-10-24 17:12:59 +0800

    delete authorization id

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const KEY = "AIzaSyCUTKRHd-Z5L0okXpyWXqLbn7e5Uv938wo"; // console.developers.google.com
+const KEY = "xxxx"; // console.developers.google.com
 
 export default axios.create({
 	baseURL: "https://www.googleapis.com/youtube/v3",
